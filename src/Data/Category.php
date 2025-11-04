6c8451c06130785b20046d7374bc23ef77a0f229
PhilippReisigl <info@easyoutdooroffice.com>
2025-11-04 17:23:52 +0100

    WIP

---
+++ src/Data/Category.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Data;
+
+use Atk4\Data\Model;
+use Expenses\Data\Expense;
+
+class Category extends Model
+{
+    public $table = 'category';
+    public $caption = 'Kategorie';
+
+    protected function init(): void
+    {
+        parent::init();
+        $this->addField(
+            'name',
+            ['type' => 'date', 'caption' => 'Datum']);
+        $this->addField(
+            'description',
+            ['type' => 'string', 'caption' => 'Beschreibung (optional)']
+        );
+
+        $this->hasMany(Expense::class, ['model' => [Expense::class]]);
+    }
+}
